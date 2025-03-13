42d87f29d1b6ada8d9331edc7f302d07a69c116e
Tristan Faure <tristan.faure@epita.fr>
2025-03-13 19:42:02 +0100

    feat(card game): added a simple card game made in XCODE for IOS

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,16 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var card1 = "back"
+    @State var card2 = "back"
+    @State var tie = ""
+    @State var player = 0
+    @State var CPU = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                Text(tie)
+                    .font(.title)
+                    .bold()
+                    .padding(.bottom, 10)
+                    .foregroundStyle(.white)
+                HStack {
+                    Spacer()
+                    Image(card1)
+                    Spacer()
+                    Image(card2)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    shuffled()
+                }) {
+                    Image("button")
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 10)
+                        Text(String(player))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 10)
+                        Text(String(CPU))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    func shuffled() {
+        let i1 = Int.random(in: 2...14)
+        let i2 = Int.random(in: 2...14)
+        card1 = "card" + String(i1)
+        card2 = "card" + String(i2)
+        if i1 > i2 {
+            tie = ""
+            player += 1
+        } else if i1 < i2 {
+            tie = ""
+            CPU += 1
+        } else {
+            tie = "It's a tie!"
+        }
     }
 }
+
 
 #Preview {
     ContentView()
